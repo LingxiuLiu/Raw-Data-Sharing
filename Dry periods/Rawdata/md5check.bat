1d71591535bbc1b0172a7fb95b659b0efdebcdd5
LingxiuLiu <liulx1001@zjnu.edu.cn>
2024-06-15 15:21:39 +0800

    Add files via upload

---
+++ Dry periods/Rawdata/md5check.bat
@@ -0,0 +1,5 @@
+
+Echo full path £º %~f0£»
+md5sum.exe -c md5.md5
+echo md5check finished£¬please confirm£¡
+pause
---
+++ Normal periods/Rawdata/md5out.bat
@@ -0,0 +1,5 @@
+
+Echo full path £º %~f0
+md5sum.exe *.gz *.pdf *.zip *.rar *.txt *.xlsx *.docs>md5.md5
+echo md5code file is created£¬please close it.
+pause
